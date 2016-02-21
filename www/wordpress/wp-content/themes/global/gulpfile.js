8c72ded29b916b697b548b25800e715bcbaecf0f
Gromek Marcin <marcin.gromek@gracjankleger.com>
2016-02-21 14:28:19 +0100

    files changes

--- www/wordpress/wp-content/themes/global/gulpfile.js
+++ www/wordpress/wp-content/themes/global/gulpfile.js
@@ -1,100 +1,34 @@
-// Include gulp.
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var config = require('./config.json');
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var reload      = browserSync.reload;
+var sass        = require('gulp-sass');
 
-// Include plugins.
-var sass = require('gulp-sass');
-var imagemin = require('gulp-imagemin');
-var pngcrush = require('imagemin-pngcrush');
-var shell = require('gulp-shell');
-var plumber = require('gulp-plumber');
-var notify = require('gulp-notify');
-var autoprefix = require('gulp-autoprefixer');
-var glob = require('gulp-sass-glob');
-var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
-var rename = require('gulp-rename');
-var sourcemaps = require('gulp-sourcemaps');
+// browser-sync task for starting the server.
+gulp.task('browser-sync', function() {
+    //watch files
+    var files = [
+    './style.css',
+    './*.php'
+    ];
 
-// CSS.
-gulp.task('css', function() {
-  return gulp.src(config.css.src)
-    .pipe(glob())
-    .pipe(plumber({
-      errorHandler: function (error) {
-        notify.onError({
-          title:    "Gulp",
-          subtitle: "Failure!",
-          message:  "Error: <%= error.message %>",
-          sound:    "Beep"
-        }) (error);
-        this.emit('end');
-      }}))
-    .pipe(sourcemaps.init())
-    .pipe(sass({
-      style: 'compressed',
-      errLogToConsole: true,
-      includePaths: config.css.includePaths
-    }))
-    .pipe(autoprefix('last 2 versions', '> 1%', 'ie 9', 'ie 10'))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(config.css.dest));
+    //initialize browsersync
+    browserSync.init(files, {
+    //browsersync with a php server
+    proxy: "http://ksostrovia.dev",
+    notify: false,
+    });
 });
 
-// JS
-gulp.task('js', function() {
-  return gulp.src(config.js.src)
-    .pipe(sourcemaps.init())
-    .pipe(concat(config.js.file))
-    .pipe(plumber({
-      errorHandler: function (error) {
-        notify.onError({
-          title:    "JS",
-          subtitle: "Failure!",
-          message:  "Error: <%= error.message %>",
-          sound:    "Beep"
-        }) (error);
-        this.emit('end');
-      }}))
-    .pipe(uglify())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(config.js.dest));
+// Sass task, will run when any SCSS files change & BrowserSync
+// will auto-update browsers
+gulp.task('sass', function () {
+    return gulp.src('assets/sass/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./'))
+        .pipe(reload({stream:true}));
 });
 
-// Compress images.
-gulp.task('images', function () {
-  return gulp.src(config.images.src)
-    .pipe(imagemin({
-      progressive: true,
-      svgoPlugins: [{ removeViewBox: false }],
-      use: [pngcrush()]
-    }))
-    .pipe(gulp.dest(config.images.dest));
+// Default task to be run with `gulp`
+gulp.task('default', ['sass', 'browser-sync'], function () {
+    gulp.watch("assets/sass/**/*.scss", ['sass']);
 });
-
-// Fonts.
-gulp.task('fonts', function() {
-  return gulp.src(config.fonts.src)
-    .pipe(gulp.dest(config.fonts.dest));
-});
-
-// Static Server + Watch
-gulp.task('serve', ['css', 'js', 'fonts'], function() {
-  browserSync.init({
-    proxy: config.browserSyncProxy
-  });
-
-  gulp.watch(config.js.src, ['js']);
-  gulp.watch(config.css.src, ['css']);
-  gulp.watch(config.images.src, ['images']);
-  gulp.watch('assets/**/*').on('change', browserSync.reload);
-});
-
-// Run drush to clear the theme registry.
-gulp.task('drush', shell.task([
-  'drush cache-clear theme-registry'
-]));
-
-// Default Task
-gulp.task('default', ['serve']);
