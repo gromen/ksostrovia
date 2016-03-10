e3d0a343b540af084da7b2c03afb1484ce29b73c
Gromek Marcin <marcin.gromek@gracjankleger.com>
2016-03-10 18:43:05 +0100

    changes

--- www/wordpress/wp-content/themes/global/header.php
+++ www/wordpress/wp-content/themes/global/header.php
@@ -24,7 +24,7 @@
 				<div class="container-fluid">
 					<div class="container">
 						<div class="row">
-							<div class="site-navigation-inner col-sm-12">
+							<div class="col-sm-12">
 								<div class="navbar navbar-default">
 									<div class="navbar-header">
 										<!-- .navbar-toggle is used as the toggle for collapsed navbar content -->
@@ -35,7 +35,7 @@
 										<span class="icon-bar"></span>
 										</button>
 										<!-- Your site title as branding in the menu -->
-										<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php bloginfo('template_directory')?>/assets/images/logo.png" /></a>
+										<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php bloginfo('template_directory')?>/assets/images/logo.png" id="logo"/></a>
 									</div>
 									<!-- The WordPress Menu goes here -->
 									<?php wp_nav_menu(
