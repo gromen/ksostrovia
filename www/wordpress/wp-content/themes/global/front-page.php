c581446ebb40d964d92bf1b5d126b121388b4e08
Gromek Marcin <marcin.gromek@gracjankleger.com>
2016-04-02 00:22:54 +0200

    added hyperlinks to post title

--- www/wordpress/wp-content/themes/global/front-page.php
+++ www/wordpress/wp-content/themes/global/front-page.php
@@ -11,19 +11,21 @@
     <article class="news-post clearfix<?php if($count == 1): ?> first<?php endif; ?>">
       <div class="news-post__content">
         <?php if ( has_post_thumbnail() && $count == 1 ) ://display image size 700x250px for first Post ?>
-            <figure class="news-post__image"> <?php the_post_thumbnail('img750x250'); ?></figure>
+            <figure class="news-post__image"> <?php the_post_thumbnail('img750x250'); ?>
+            </figure>
 
         <?php else : //if is not first post, display 250x250px image size?>
-            <figure class="news-post__image"> <?php the_post_thumbnail('img250x250'); ?></figure>
+            <figure class="news-post__image"> <?php the_post_thumbnail('img250x250'); ?>
+            </figure>
         <?php endif; ?>
 
 
           <div class="news-post__text-container <?php if(!has_post_thumbnail()) : ?>news-post__text-container--full-width<?php endif; ?>">
 
 
-          <h3 class="news-post__title h3"><?php the_title(); ?></h3>
+          <h3 class="news-post__title h3"><a href="<?php the_permalink() ?>" title="Read more <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
           <span class="news-post__published-date"><?php the_date(); ?></span>
-          <div class="news-post__body"><?php echo get_the_excerpt(); ?></div>
+          <p class="news-post__body"><?php echo get_the_excerpt(); ?></p>
         </div>
       </div>
     </article>
